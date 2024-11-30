1bb12f3f2904a2ef1ff4a2f28afb9058c6035562
dvir_levy <69078741+jarman155@users.noreply.github.com>
2024-11-30 16:30:43 +0200

    finished the constants

--- src/main/java/frc/robot/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Intake/IntakeConstants.java
@@ -7,10 +7,46 @@ package frc.robot.Intake;
 /** Add your docs here. */
 public final class IntakeConstants {
     public static class idMotors{
-        public static final String CANBUS = "canivore";
-        public static final int idMotorfeed = 2;//TODO:"change to corect id when I know"
-        public static final int idMotorIntakeToShooter = 3;//TODO:"change to corect id when I know"
-        
+        public static final String CANBUS = "rio";
+        public static final int idMotorfeed = 40;
+        public static final int idMotorIntakeToShooter = 42;
+        public static final int idIRSensor = 3;
+    }
+    
+      /**The voltege of the sensor when he detects the note */
+    public static final double NOTE_VOLTEGE = 3.3;
+
+    //The amper of when the not is fed to the intake
+    public static final double NOTE_AMPER = 25.0;
+    public static final double NOTE_AMPER2 = 25.0;
+
+    /**The amount of time the intake command will auto stop (in miliseconds) */
+    public static final double STOP_COMMAND_TIME = 20000;
+
+    /**The amount of time the intake command will stop after takinng note */
+    public static final double STOP_AFTER_NOTE = 10;
+
+    /**
+    * The current position of the note to power fo the motors
+    */
+    public enum NotePosition{
+        NO_NOTE(1, 0.3),
+        FIRST_TOUCH(1, 0.3),
+        AFTER_SEEING_NOTE(1, 0.3);
+    
+        /** The voltage to the motors */
+        public final double pickUpPow;
+        public final double movePow;
+    
+        /**
+         * Constractor for enum
+         * @param movePow The voltage to the motors thst you get
+         */
+        NotePosition(double pickUpPow, double movePow) {
+          this.movePow = movePow;
+          this.pickUpPow = pickUpPow;
+        }
     }
 }
+
 
