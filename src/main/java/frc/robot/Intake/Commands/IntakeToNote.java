c5bbb923ce42467e9d5fce8b73f704fb0927e596
dvir_levy <69078741+jarman155@users.noreply.github.com>
2024-11-30 16:30:05 +0200

    finished the command now all i need is to know when the note is not centered

--- src/main/java/frc/robot/Intake/Commands/IntakeToNote.java
+++ src/main/java/frc/robot/Intake/Commands/IntakeToNote.java
@@ -4,29 +4,91 @@
 
 package frc.robot.Intake.Commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Intake.IntakeConstants;
+import frc.robot.Intake.IntakeConstants.NotePosition;
+import frc.robot.Intake.Subsystem.IntakeSubsystem;
 
 public class IntakeToNote extends Command {
-  /** Creates a new IntakeToNote. */
-  public IntakeToNote() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private IntakeSubsystem intakeSubsystem;
+  private boolean isNoteCentered = false;
+  /**timer for the command */
+  Timer timerCommand;
+  /**timer for intake motors to move the note */
+  Timer timerIntake;
+
+  boolean hasTaken;
+  boolean touchedDownWheels = false;
+
+  /**
+   * Takes the intake subsystem
+   * @param intakeSubsystem2
+   */
+  public IntakeToNote(IntakeSubsystem intakeSubsystem2) {
+    this.intakeSubsystem = intakeSubsystem2;
+    timerCommand = new Timer();
+    timerIntake = new Timer();
+    hasTaken = false;
+    addRequirements(intakeSubsystem);
   }
-// hi
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void initialize() {
+    intakeSubsystem.currentPosition = NotePosition.NO_NOTE;
+    timerCommand.start();
+    timerIntake.reset();
+    timerIntake.stop();
+    hasTaken = false;
+    touchedDownWheels = false;
+  }
 
-  // Called once the command ends or is interrupted.
+
+    /**
+   * Case for no note in intake, set motors to almost max
+   * <br></br> case for note touching first motor pic up , Set motors to FIRST_TOUCH powers
+   */
   @Override
-  public void end(boolean interrupted) {}
+  public void execute() {
+    if(!touchedDownWheels){
+      //TODO:make a comand in vision that you can check if the note is centered in the fase when the note is not tuched the intake
+    }
+    if (intakeSubsystem.AmperHighMotorPickUp()) {
+      intakeSubsystem.currentPosition = NotePosition.FIRST_TOUCH;
+      touchedDownWheels = true;
+    }
+    if(touchedDownWheels && intakeSubsystem.AmperHighMotorPickUp2()) {
+      intakeSubsystem.isNoteInIntake = true;
+      timerIntake.reset();
+      timerIntake.start();
+      touchedDownWheels = false;
+    } 
+    
+  }
 
-  // Returns true when the command should end.
+
+    /**
+   * When the funcation end we updated the boolean vlaue 
+   * <br></br> of is note in intake , turn the motors to 0 , reset the timer and center the note if needed
+   */
+  @Override
+  public void end(boolean interrupted) {
+    intakeSubsystem.setPowerMotors(0);
+    timerCommand.stop();
+    timerCommand.reset();
+    timerIntake.stop();
+    timerIntake.reset();
+    if(!isNoteCentered){
+      intakeSubsystem.centerNote();
+    }
+  }
+
+
+    /**
+   * Case when the note is in the intake
+   */
   @Override
   public boolean isFinished() {
-    return false;
+    return timerIntake.get()*1000 > IntakeConstants.STOP_AFTER_NOTE || timerCommand.get()*1000 > IntakeConstants.STOP_COMMAND_TIME;
   }
 }
