1e6e6cec83c59228bac42b87767dbd324c83a973
nanonoam <97607867+nanonoam@users.noreply.github.com>
2025-01-01 15:08:00 +0200

    fuck go buck

---
+++ src/main/java/frc/robot/chassis/ChassisConstants.java
@@ -0,0 +1,107 @@
+package frc.robot.chassis;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.utils.CancoderConfig;
+import frc.robot.utils.TalonConfig;
+
+public class ChassisConstants {
+    public static final double MAX_DRIVE_VELOCITY = 4.1;
+    public static final double MAX_OMEGA_VELOCITY = Math.toRadians(360);
+    public static final int GYRO_ID = 14;
+    public static final String BUS = "canivore";
+    public static final double WHEEL_DIAMETER = 0.1016; // 4 inch
+    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+    public static final double STEER_GEAR_RATIO = 12.8;
+    public static final double DRIVE_GEAR_RATIO = 8.14;
+    
+    public static class SwerveModuleConfigs {
+        public final TalonConfig STEER_CONFIG;
+        public final TalonConfig DRIVE_CONFIG;
+        public final CancoderConfig CANCODER_CONFIG;
+        public final Translation2d POSITION;
+        public final double STEER_OFFSET;
+
+        public SwerveModuleConfigs(TalonConfig steerConfig, TalonConfig driveConfig, CancoderConfig cancoderConfig, Translation2d position, double steerOffset) {
+            STEER_CONFIG = steerConfig;
+            DRIVE_CONFIG = driveConfig;
+            CANCODER_CONFIG = cancoderConfig;
+            POSITION = position;
+            STEER_OFFSET = steerOffset;
+        }
+    }
+    
+    public static final double STEER_KP = 12;
+    public static final double STEER_KI = 0;
+    public static final double STEER_KD = 0;
+    public static final double STEER_KS = 0.2;
+    public static final double STEER_KV = 0.24;
+    public static final double STEER_KA = 0.002;
+
+    public static final double DRIVE_KP = 0.06;
+    public static final double DRIVE_KI = 0;
+    public static final double DRIVE_KD = 0.004;
+    public static final double DRIVE_KS = 0.2;
+    public static final double DRIVE_KV = 0.45;
+    public static final double DRIVE_KA = 0.004;
+
+    public static final double MOTION_MAGIC_VEL = 15  * 2 * Math.PI;
+    public static final double MOTION_MAGIC_ACCEL = 40 * 2 * Math.PI;
+    public static final double MOTION_MAGIC_JERK = 100 * 2 * Math.PI;
+
+    public static final SwerveModuleConfigs FRONT_LEFT = new SwerveModuleConfigs(
+        new TalonConfig(5, BUS, "Front Left Steer")
+            .withPID(STEER_KP, STEER_KI, STEER_KD, STEER_KS, STEER_KV, 0, 0)
+            .withMotionMagic(MOTION_MAGIC_VEL, MOTION_MAGIC_ACCEL, MOTION_MAGIC_JERK)
+            .withBrake(true).withMotorRatio(STEER_GEAR_RATIO).withRadiansMotor(),
+        new TalonConfig(4, BUS, "Front Left Drive")
+            .withPID(DRIVE_KP, DRIVE_KI, DRIVE_KD, DRIVE_KS, DRIVE_KV, DRIVE_KA, 0)
+            .withBrake(true)
+            .withInvert(true)
+            .withMotorRatio(DRIVE_GEAR_RATIO).withMeterMotor(WHEEL_CIRCUMFERENCE),
+        new CancoderConfig(6, BUS, "Front Left Cancoder"),
+        new Translation2d(0.266, 0.249),
+        -0.233
+    );
+    public static final SwerveModuleConfigs FRONT_RIGHT = new SwerveModuleConfigs(
+        new TalonConfig(2, BUS, "Front Right Steer")
+            .withPID(STEER_KP, STEER_KI, STEER_KD, STEER_KS, STEER_KV, 0, 0)
+            .withMotionMagic(MOTION_MAGIC_VEL, MOTION_MAGIC_ACCEL, MOTION_MAGIC_JERK)
+            .withBrake(true).withMotorRatio(STEER_GEAR_RATIO).withRadiansMotor(),
+        new TalonConfig(1, BUS, "Front Right Drive")
+            .withPID(DRIVE_KP, DRIVE_KI, DRIVE_KD, DRIVE_KS, DRIVE_KV, DRIVE_KA, 0)
+            .withBrake(true)
+            .withInvert(true)
+            .withMotorRatio(DRIVE_GEAR_RATIO).withMeterMotor(WHEEL_CIRCUMFERENCE),
+        new CancoderConfig(3, BUS, "Front Right Cancoder"),
+        new Translation2d(0.266, -0.249),
+        2.01
+    );
+    public static final SwerveModuleConfigs BACK_LEFT = new SwerveModuleConfigs(
+        new TalonConfig(11, BUS, "Back Left Steer")
+            .withPID(STEER_KP, STEER_KI, STEER_KD, STEER_KS, STEER_KV, 0, 0)
+            .withMotionMagic(MOTION_MAGIC_VEL, MOTION_MAGIC_ACCEL, MOTION_MAGIC_JERK)
+            .withBrake(true).withMotorRatio(STEER_GEAR_RATIO).withRadiansMotor(),
+        new TalonConfig(10, BUS, "Back Left Drive")
+            .withPID(DRIVE_KP, DRIVE_KI, DRIVE_KD, DRIVE_KS, DRIVE_KV, DRIVE_KA, 0)
+            .withBrake(true)
+            .withInvert(true)
+            .withMotorRatio(DRIVE_GEAR_RATIO).withMeterMotor(WHEEL_CIRCUMFERENCE),
+        new CancoderConfig(12, BUS, "Back Left Cancoder"),
+        new Translation2d(-0.266, 0.249),
+        1.01
+    );
+    public static final SwerveModuleConfigs BACK_RIGHT = new SwerveModuleConfigs(
+        new TalonConfig(8, BUS, "Back Right Steer")
+            .withPID(STEER_KP, STEER_KI, STEER_KD, STEER_KS, STEER_KV, 0, 0)
+            .withMotionMagic(MOTION_MAGIC_VEL, MOTION_MAGIC_ACCEL, MOTION_MAGIC_JERK)
+            .withBrake(true).withMotorRatio(STEER_GEAR_RATIO).withRadiansMotor(),
+        new TalonConfig(7, BUS, "Back Right Drive")
+            .withPID(DRIVE_KP, DRIVE_KI, DRIVE_KD, DRIVE_KS, DRIVE_KV, DRIVE_KA, 0)
+            .withBrake(true)
+            .withInvert(true)
+            .withMotorRatio(DRIVE_GEAR_RATIO).withMeterMotor(WHEEL_CIRCUMFERENCE),
+        new CancoderConfig(9, BUS, "Back Right Cancoder"),
+        new Translation2d(-0.266, -0.249),
+        0.977
+    );
+}
---
+++ src/main/java/frc/robot/chassis/subsystems/Chassis.java
@@ -0,0 +1,110 @@
+package frc.robot.chassis.subsystems;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.Sysid.Sysid;
+import frc.robot.chassis.ChassisConstants;
+
+public class Chassis extends SubsystemBase {
+    private SwerveModule[] modules;
+    private Pigeon2 gyro;
+    private SwerveDriveKinematics kinematics;
+    private SwerveDrivePoseEstimator poseEstimator;
+    private Field2d field;
+
+    public Chassis() {
+        modules = new SwerveModule[] {
+            new SwerveModule(ChassisConstants.FRONT_LEFT),
+            new SwerveModule(ChassisConstants.FRONT_RIGHT),
+            new SwerveModule(ChassisConstants.BACK_LEFT),
+            new SwerveModule(ChassisConstants.BACK_RIGHT),
+        };
+        gyro = new Pigeon2(ChassisConstants.GYRO_ID, ChassisConstants.BUS);
+
+        kinematics = new SwerveDriveKinematics(
+            ChassisConstants.FRONT_LEFT.POSITION,
+            ChassisConstants.FRONT_RIGHT.POSITION,
+            ChassisConstants.BACK_LEFT.POSITION,
+            ChassisConstants.BACK_RIGHT.POSITION
+
+        );
+        poseEstimator = new SwerveDrivePoseEstimator(kinematics, getGyroAngle(), getModulePositions(), new Pose2d());
+        field = new Field2d();
+
+    }
+
+    public void setVelocities(ChassisSpeeds speeds) {
+        speeds = ChassisSpeeds.fromFieldRelativeSpeeds(speeds, getGyroAngle());
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speeds);
+        setModuleStates(states);
+    }
+
+    public void setSteerPositions(double[] positions) {
+        for (int i = 0; i < positions.length; i++) {
+            modules[i].setSteerPosition(positions[i]);
+        }
+    }
+
+    public void setSteerPower(double pow, int id){
+        modules[id].setSteerPower(pow);
+    }
+
+    public double getSteerVelocity(int id){
+        return modules[id].getSteerVel();
+    }
+    public double getSteeracceleration(int id){
+        return modules[id].getSteeraccel();
+    }
+
+    public void setSteerPositions(double position) {
+        setSteerPositions(new double[] { position, position, position, position});
+    }
+
+
+    public void setDriveVelocities(double[] velocities) {
+        for (int i = 0; i < velocities.length; i++) {
+            modules[i].setDriveVelocity(velocities[i]);
+        }
+    }
+
+    public void setDriveVelocities(double velocity) {
+        setDriveVelocities(new double[] { velocity, velocity, velocity, velocity});
+    }
+
+    public Rotation2d getGyroAngle() {
+        return Rotation2d.fromRadians(gyro.getAngle());
+    }
+
+    private SwerveModulePosition[] getModulePositions() {
+        SwerveModulePosition[] arr = new SwerveModulePosition[modules.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = modules[i].getModulePosition();
+        }
+        return arr;
+    }
+
+    private void setModuleStates(SwerveModuleState[] states) {
+        for (int i = 0; i < states.length; i++) {
+            modules[i].setState(states[i]);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        poseEstimator.update(getGyroAngle(), getModulePositions());
+        field.setRobotPose(poseEstimator.getEstimatedPosition());
+    }
+        public boolean isRed() {
+        return RobotContainer.isRed();
+    }
+}
