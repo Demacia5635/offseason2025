f21638d08a43c1adce4403e582b8bf21c169572d
shayFarj <shayfarjoon0@gmail.com>
2025-01-12 20:15:57 +0200

    Testing stationnav : creating obstacle

--- src/main/java/frc/robot/PathFollow/Util/PathsConstants.java
+++ src/main/java/frc/robot/PathFollow/Util/PathsConstants.java
@@ -5,6 +5,8 @@
 package frc.robot.PathFollow.Util;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 /** Add your docs here. */
 public class PathsConstants {
@@ -19,19 +21,22 @@ public class PathsConstants {
     public final static double MIN_SEGMENT_LENGTH = 0.15;
     public final static double MAX_RADIAL_ACCEL = -1;
 
+
+    public final static Translation2d STATION_CENTER = new Translation2d(4,4);
+    public final static double STATION_RADIUS = 4;
     public final static Pose2d[] STATIONS = new Pose2d[]{
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d(),
-        new Pose2d()
+        new Pose2d(new Translation2d(1,0).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(0)),
+        new Pose2d(new Translation2d(0.866,-0.5).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12) - Math.PI)),
+        new Pose2d(new Translation2d(0.5,-0.866).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*2 - Math.PI)),
+        new Pose2d(new Translation2d(0,-1).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*3 - Math.PI)),
+        new Pose2d(new Translation2d(-0.5,-0.866).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*4 - Math.PI)),
+        new Pose2d(new Translation2d(-0.866,-0.5).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*5 - Math.PI)),
+        new Pose2d(new Translation2d(-1,0).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*6 - Math.PI)),
+        new Pose2d(new Translation2d(-0.866,0.5).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*7 - Math.PI)),
+        new Pose2d(new Translation2d(-0.5,0.866).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*8 - Math.PI)),
+        new Pose2d(new Translation2d(0,1).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*9 - Math.PI)),
+        new Pose2d(new Translation2d(0.5,0.866).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*10 - Math.PI)),
+        new Pose2d(new Translation2d(0.866,0.5).times(STATION_RADIUS).plus(STATION_CENTER),new Rotation2d(((Math.PI*2)/12)*11 - Math.PI))
     }; //please arrange it clockwise
 
 }
